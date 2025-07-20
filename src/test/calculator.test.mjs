ad08ec0bb6c9e986803d9339ace3cba8cf9e659f
Prajwal Jaswal <prajwaljaswal@Prajwals-MacBook-Air.local>
2025-07-20 16:06:19 +0530

    should throw on multiple negative numbers

--- src/test/calculator.test.mjs
+++ src/test/calculator.test.mjs
@@ -27,5 +27,9 @@ expect(add("//;\n1;2")).toBe(3);
 test('should throw on single negative number', () => {
 expect(() => add("1,-2")).toThrow("negative numbers not allowed -2");
 });
+
+test('should throw on multiple negative numbers', () => {
+expect(() => add("1,-2,-3")).toThrow("negative numbers not allowed -2,-3");
+});
  
 });
