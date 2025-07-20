ef6eb014073c52344bccd26db47d86fedd53c9ed
Prajwal Jaswal <prajwaljaswal@Prajwals-MacBook-Air.local>
2025-07-20 16:04:36 +0530

    should support new line as delimiter

--- src/test/calculator.test.mjs
+++ src/test/calculator.test.mjs
@@ -17,5 +17,9 @@ test('should handle unknown amount of numbers', () => {
 expect(add("1,2,3,4")).toBe(10);
 });
 
+test('should support new line as delimiter', () => {
+expect(add("1\n2,3")).toBe(6);
+});
+
  
 });
