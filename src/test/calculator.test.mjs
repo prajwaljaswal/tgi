5e396d896960d143e6633ef0f057665fad947f9d
Prajwal Jaswal <prajwaljaswal@Prajwals-MacBook-Air.local>
2025-07-20 16:01:55 +0530

    should return numbber for singlr input

--- src/test/calculator.test.mjs
+++ src/test/calculator.test.mjs
@@ -4,5 +4,10 @@ describe('String Calculator', () => {
   test('should return 0 for empty string', () => {
     expect(add("")).toBe(0);
   });
+
+  test('should return number for single input', () => {
+expect(add("1")).toBe(1);
+});
+
  
 });
